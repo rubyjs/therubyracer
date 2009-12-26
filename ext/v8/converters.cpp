1b85fd26d8ae09f3d9d708128802fe23b2a54c14
Bill Robertson <billrobertson42@gmail.com>
2009-12-25 22:16:11 -0500

    commit to branch before switching back to master to make it compile.

--- ext/v8/converters.cpp
+++ ext/v8/converters.cpp
@@ -1,4 +1,21 @@
 #include "converters.h"
 
-convert_v8_to_rb_t V82RB;
-convert_rb_to_v8_t RB2V8;
+VALUE V82RB(v8::Handle<v8::Value>& value) {
+  convert_v8_to_rb_t convert;
+  return convert(value);
+}
+
+v8::Local<v8::Value> RB2V8(VALUE value) {
+  convert_rb_to_v8_t convert;
+  return convert(value);
+}
+
+std::string RB2String(VALUE value) {
+  convert_rb_to_string_t convert;
+  return convert(value);
+}
+
+std::string V82String(v8::Handle<v8::Value>& value) {
+  convert_v8_to_string_t convert;
+  return convert(value);
+}
