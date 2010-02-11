a44675f70ace898e3db82f02f035f3464c4a4b21
Charles Lowell <cowboyd@thefrontside.net>
2010-02-11 16:42:40 -0600

    native eval now takes a source parameter.

--- spec/ext/obj_spec.rb
+++ spec/ext/obj_spec.rb
@@ -5,7 +5,7 @@ include V8
 describe C::Object do
   it "has a reference to its calling context" do
     C::Context.new.open do |cxt|
-      o = cxt.eval('new Object()');
+      o = cxt.eval('new Object()', '<eval>');
       o.context.should == cxt
       o.context.should be_eql(cxt)
     end
