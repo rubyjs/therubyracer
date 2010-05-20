9a44d204c8636ae6f54345190e29ff28fbbefd73
Charles Lowell <cowboyd@thefrontside.net>
2010-05-20 11:24:27 +0300

    allow v8 refs to hold on to more than one ruby object.

--- ext/v8/v8_ref.cpp
+++ ext/v8/v8_ref.cpp
@@ -2,25 +2,38 @@
 #include "stdio.h"
 using namespace v8;
 
+v8_ref::v8_ref(Handle<void> object, VALUE ref) : handle(Persistent<void>::New(object)) {
+  references = rb_hash_new();
+  this->add(ref);
+}
 
-v8_ref::v8_ref(Handle<void> object, VALUE ref) : handle(Persistent<void>::New(object)), references(ref) {
-
+void v8_ref::add(VALUE ref) {
+  if (ref != 0 && RTEST(ref)) {
+    rb_hash_aset(references, ref, Qtrue);
+  }
 }
 
 v8_ref::~v8_ref() {  
   handle.Dispose();
 }
 
-void v8_ref_mark(v8_ref* ref) {
-  if (ref->references != 0) {
+namespace {
+  //the v8_ref wraps a v8 handle so that ruby can hold a reference to it.
+  void gc_mark(v8_ref* ref) {
     rb_gc_mark(ref->references);
+  }
+
+  void gc_free(v8_ref* ref) {
+    delete ref;
   }
 }
 
-void v8_ref_free(v8_ref* ref) {
-  delete ref;
+void rr_ref_addref(VALUE data, VALUE newref) {
+  v8_ref * ref = 0;
+  Data_Get_Struct(data, struct v8_ref, ref);
+  ref->add(newref);
 }
 
 VALUE V8_Ref_Create(VALUE ruby_class, v8::Handle<void> handle, VALUE ref) {
-  return Data_Wrap_Struct(ruby_class, v8_ref_mark, v8_ref_free, new v8_ref(handle, ref));
+  return Data_Wrap_Struct(ruby_class, gc_mark, gc_free, new v8_ref(handle, ref));
 }
