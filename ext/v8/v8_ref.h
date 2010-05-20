f1222786cfc8b7e37e56bc46c6d4511a16993b1c
Charles Lowell <cowboyd@thefrontside.net>
2010-05-20 11:30:27 +0300

    Revert "allow v8 refs to hold on to more than one ruby object."
    This reverts commit 9a44d204c8636ae6f54345190e29ff28fbbefd73.

--- ext/v8/v8_ref.h
+++ ext/v8/v8_ref.h
@@ -4,17 +4,21 @@
 #include <v8.h>
 #include "ruby.h"
 
+//the v8_ref wraps a v8 handle so that ruby can hold a reference to it.
+
 struct v8_ref  {
   //takes a handle object and adds a new persistent handle for 
   //the referenced object
   v8_ref(v8::Handle<void> object, VALUE ref = 0);
   virtual ~v8_ref();
-  void add(VALUE ref);
   v8::Persistent<void> handle;
   VALUE references;
 };
 
-void rr_ref_addref(VALUE handle, VALUE newref);
+
+//memory management
+void v8_ref_mark(v8_ref* ref);
+void v8_ref_free(v8_ref* ref);
 
 VALUE V8_Ref_Create(VALUE ruby_class, v8::Handle<void> handle, VALUE ref = 0);
 
