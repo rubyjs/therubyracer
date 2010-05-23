99faaad1bee056b8b0ed3a859dacda63dfff86fe
Charles Lowell <cowboyd@thefrontside.net>
2010-05-23 04:01:08 +0300

    don't print stack trace for a fatal error. Only a segfault

--- ext/v8/v8_exception.cpp
+++ ext/v8/v8_exception.cpp
@@ -8,13 +8,10 @@ namespace {
   static void* stack[20];
 
   void fatal(const char* location, const char* message) {
-    int size = backtrace(stack, 20);
-    backtrace_symbols_fd(stack, size, 2);
     rb_raise(rb_eFatal, "%s: %s", location, message);    
   }
   void segfault(int sig) {
     fprintf(stderr, "segfault: game over.\n");
-    void *elements[20];
     int size = backtrace(stack, 20);
     backtrace_symbols_fd(stack, size, 2);
     exit(1);
