00dad84dcf07da34292b3d5bc03229c4c108e5cf
Charles Lowell <cowboyd@thefrontside.net>
2010-05-27 19:30:43 +0300

    access check not necessary if it doesn't matter if we're in contexts anymore

--- spec/ext/cxt_spec.rb
+++ spec/ext/cxt_spec.rb
@@ -8,12 +8,5 @@ describe C::Context do
   it "should not have a current context if no context is open" do
     C::Context::GetEntered().should be_nil
   end
-  
-  it "can determine if there is a current context" do
-    C::Context::InContext().should be(false)
-    Context.open do
-      C::Context::InContext().should be(true)
-    end
-  end
-    
+
 end
