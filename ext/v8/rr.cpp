8a7965c6e5b085ce9a4981a47b8841ef29edf7b6
Charles Lowell <cowboyd@thefrontside.net>
2010-06-18 14:44:44 +0300

    remove html from warning message

--- ext/v8/rr.cpp
+++ ext/v8/rr.cpp
@@ -143,10 +143,10 @@ Handle<Value> rr_rb2v8(VALUE value) {
 //  case T_SCOPE: (not in 1.9)
   case T_NODE:  
   default:
-    rb_warn("unknown conversion to V8 for: %s<br/>", RSTRING_PTR(rb_inspect(value)));
+    rb_warn("unknown conversion to V8 for: %s", RSTRING_PTR(rb_inspect(value)));
     return String::New("Undefined Conversion");
   }
-  
+
   return Undefined();
 }
 // VALUE rr_v82rb(v8::ScriptData *data) {
