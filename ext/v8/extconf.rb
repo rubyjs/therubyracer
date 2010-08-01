706b4e961524dc3ac9d74b795a7a521948468dcd
Charles Lowell <cowboyd@thefrontside.net>
2010-08-01 09:48:25 -0500

    remove obsolete tasks from the Makefile

--- ext/v8/extconf.rb
+++ ext/v8/extconf.rb
@@ -23,21 +23,3 @@ $LIBS << ' -lv8'
 CONFIG['LDSHARED'] = '$(CXX) -shared' unless RUBY_PLATFORM =~ /darwin/
 
 create_makefile('v8')
-
-# now add a few extra targets
-File.open("Makefile", "a") do |makefile|
-makefile.print <<EOF
-
-test: all
-	@echo running spec...
-	spec -O spec/spec.opts spec/therubyracer_spec.rb
-
-docs/cpp:
-	mkdir -p docs/cpp
-
-docs: all docs/cpp
-	@echo Generate C++ docs to docs/cpp/html
-	doxygen Doxyfile
-
-EOF
-end
