8443e82d31f00d0068c99eb80b3f4c266b478b1a
Brendan Grainger <brendan.grainger@gmail.com>
2011-01-06 21:46:44 -0500

    Fixed therubyracer.gemspec conflict

---
+++ spec/v8/context_spec.rb
@@ -0,0 +1,16 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe V8::Context do
+  # before(:each) do
+  #    @cxt = V8::Context.new
+  #    @cxt['one'] = lambda do
+  #      @cxt.eval('two()', 'one.js')
+  #    end
+  #    @cxt['two'] = lambda do
+  #      @cxt.eval('three()', 'two.js')
+  #    end
+  #  end
+   
+
+   
+end
