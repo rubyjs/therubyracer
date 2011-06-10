a6a0ed0d0cc02c233ff9ede5cb9336274097419f
Charles Lowell <cowboyd@thefrontside.net>
2011-06-10 09:51:53 -0500

    manually clear references if a proxy has been garbage collected, but its finalizer has not yet run.

--- lib/v8/portal/proxies.rb
+++ lib/v8/portal/proxies.rb
@@ -76,9 +76,16 @@ module V8
         if id = @rb_proxies_js2rb[object]
           ObjectSpace._id2ref id
         end
+      rescue RangeError => e
+        # sometimes, the Ruby proxy has been garbage collected, but
+        # the finalizer which runs has not been called. That's OK
+        # we just clear out the entry, and return nil so that a new
+        # proxy can be created.
+        @clear_rb_proxy.call(id)
+        return nil
       end
 
-      # Looks up the natively JavaScript object by its Ruby proxy
+      # Looks up a native JavaScript object by its Ruby proxy
       # @param [Object] proxy the Ruby proxy
       # @return [V8::C::Handle] the native JavaScript object
       def rb_proxy_2_js_object(proxy)
@@ -112,16 +119,35 @@ module V8
         end
       end
 
+      # @Private
+      # Remove the linkage between a Ruby proxy and a native
+      # JavaScript object. In general, this object is registered
+      # as a finalizer on the Ruby proxy itself, so that when it is
+      # garbage collected, it releases the back reference to the
+      # native JavaScript object.
+      #
+      # It is important to do this as soon as is reasonably possible
+      # so that the native JavaScript object can itself be garbage 
+      # collected (provided there are no other references to it)
       class ClearRubyProxy
         def initialize(rb2js, js2rb)
           @rb2js, @js2rb = rb2js, js2rb
         end
 
+        # takes the object id of a Ruby proxy that has been garbage collected
+        # and releases the reference to the native JavaScript object that
+        # it was bound to.
+        # @param[Fixnum] proxy_id the proxy id of the garbage collected Ruby proxy
         def call(proxy_id)
-          js = @rb2js[proxy_id]
-          @rb2js.delete(proxy_id)
-          @js2rb.delete(js)
-          V8::C::V8::AdjustAmountOfExternalAllocatedMemory(-8 * 1024)
+          # TODO: this if-check should be synchronized, so that if called manually
+          # it will not conflict with the finalization thread. It's not so heinous
+          # if the refererence gets cleared twice, but we definiteily dont't want
+          # to double-decrement the v8 GC hint.
+          if js = @rb2js[proxy_id]
+            @rb2js.delete(proxy_id)
+            @js2rb.delete(js)
+            V8::C::V8::AdjustAmountOfExternalAllocatedMemory(-8 * 1024)
+          end
         end
       end
     end
