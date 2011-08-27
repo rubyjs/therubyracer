eabda4435e04b5d0172631ea2448a564c7bd113f
David Turnbull <dturnbull@gmail.com>
2011-08-27 16:57:45 -0700

    demonstrate failure to execute on multiple threads simultaneously

--- specthread/threading_spec.rb
+++ specthread/threading_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 
 describe "using v8 from multiple threads" do
   
-  it "is possible" do
-    2.times.collect do
+  it "creates contexts from within threads" do
+    10.times.collect do
       Thread.new do
         require 'v8'
         V8::Context.new
@@ -12,4 +12,16 @@ describe "using v8 from multiple threads" do
     end.each {|t| t.join}
     V8::Context.new
   end
+  
+  it "executes codes on multiple threads simultaneously" do
+    5.times.collect{V8::Context.new}.collect do |ctx|
+      Thread.new do
+        ctx['x'] = 99
+        while ctx['x'] > 0
+          ctx.eval 'for (i=10000;i;i--){};--x'
+        end
+      end
+    end.each {|t| t.join}
+  end
+  
 end
