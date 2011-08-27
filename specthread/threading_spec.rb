c48de96ab11431f2efa7cd1eb0a39a81bc45c643
David Turnbull <dturnbull@gmail.com>
2011-08-27 15:11:19 -0700

    demonstrate threading failure

--- specthread/threading_spec.rb
+++ specthread/threading_spec.rb
@@ -4,10 +4,12 @@ require 'spec_helper'
 describe "using v8 from multiple threads" do
   
   it "is possible" do
-    Thread.new do
-      require 'v8'
-      V8::Context.new
-    end.join
+    2.times.collect do
+      Thread.new do
+        require 'v8'
+        V8::Context.new
+      end
+    end.each {|t| t.join}
     V8::Context.new
   end
 end
