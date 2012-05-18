125a37a5d3df611524222ff5ee30aae3734ad63a
Charles Lowell <cowboyd@thefrontside.net>
2012-05-18 09:49:44 -0500

    sanity check for enumeration Object properties

--- spec/c/object_spec.rb
+++ spec/c/object_spec.rb
@@ -17,6 +17,19 @@ describe V8::C::Object do
       o.Get(key).Utf8Value().should eql "bar"
     end
   end
+
+  it "can retrieve all property names" do
+    V8::C::HandleScope() do
+      o = V8::C::Object::New()
+      {"foo" => "bar", "baz" => "bang"}.each do |key, value|
+        o.Set(V8::C::String::New(key), V8::C::String::New(value))
+      end
+      names = o.GetPropertyNames()
+      names.Length().should eql 2
+      names.Get(0).Utf8Value().should eql "foo"
+      names.Get(1).Utf8Value().should eql "baz"
+    end
+  end
   it "can set an accessor from ruby" do
     V8::C::HandleScope() do
       o = V8::C::Object::New()
