d9dea47309b07945e99becb0dc18569eeb9bac05
Charles Lowell <cowboyd@thefrontside.net>
2012-06-07 09:28:11 -0500

    give a better pending message on the redjs spec

--- spec/redjs_spec.rb
+++ spec/redjs_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 require 'redjs/load_specs'
 module RedJS
   Context = V8::Context
-  # Error = V8::JSError
+  #Error = V8::JSError
 end
 describe V8::Context do
-  pending
-  # it_behaves_like 'RedJS::Context'
+  pending "not ready for prime-time"
+  #it_behaves_like 'RedJS::Context'
 end
