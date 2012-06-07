bcabe3986682885ae45fcd894d21d792358e94f0
Charles Lowell <cowboyd@thefrontside.net>
2012-06-07 05:19:24 -0500

    make sure that simple evaluation does not leak

--- spec/mem/blunt_spec.rb
+++ spec/mem/blunt_spec.rb
@@ -2,6 +2,9 @@ require 'spec_helper'
 
 describe "A Very blunt test to make sure that we aren't doing stupid leaks" do
   before do
+    if Object.const_defined?(:RUBY_ENGINE) && RUBY_ENGINE == 'rbx'
+      pending 'need to figure out how to do memory sanity checks on rbx'
+    end
     #allocate a single context to make sure that v8 loads its snapshot and
     #we pay the overhead.
     V8::Context.new
@@ -13,14 +16,20 @@ describe "A Very blunt test to make sure that we aren't doing stupid leaks" do
     GC.stress = false
   end
   it "won't increase process memory by more than 50% no matter how many contexts we create" do
-    if Object.const_defined?(:RUBY_ENGINE) && RUBY_ENGINE == 'rbx'
-      pending 'need to figure out how to do memory sanity checks on rbx'
-    end
     500.times do
        V8::Context.new
        run_v8_gc
     end
     process_memory.should <= @start_memory * 1.5
+  end
+
+  it "can eval simple value passing statements repeatedly without significantly increasing memory" do
+    cxt = V8::Context.new
+    500.times do
+      cxt.eval('7 * 6')
+      run_v8_gc
+    end
+    process_memory.should <= @start_memory * 1.1
   end
 
   def process_memory
