ad7e9dddb70585a85ea53f153c40d54cc134deb3
Charles Lowell <cowboyd@thefrontside.net>
2012-06-11 16:21:06 -0500

    set named Ruby properties from JS

--- lib/v8/conversion/object.rb
+++ lib/v8/conversion/object.rb
@@ -2,7 +2,7 @@ class V8::Conversion
   module Object
     def to_v8
       template = V8::C::ObjectTemplate::New()
-      template.SetNamedPropertyHandler(Get, nil, nil, nil, nil, V8::C::External::New(self))
+      template.SetNamedPropertyHandler(Get, Set, nil, nil, nil, V8::C::External::New(self))
       instance = template.NewInstance()
       V8::Context.link self, instance
       return instance
@@ -19,9 +19,24 @@ class V8::Conversion
         object = info.Data().Value()
         name = property.Utf8Value()
         dontintercept = proc do
-          return V8::C::Empty
+          return V8::C::Value::Empty
         end
         context.to_v8 access.get(object, name, &dontintercept)
+      rescue Exception => e
+        warn "uncaught exception: #{e.class}: #{e.message} while accessing object property: #{e.backtrace.join('\n')}"
+      end
+    end
+
+    class Set
+      def self.call(property, value, info)
+        context = V8::Context.current
+        access = context.access
+        object = info.Data().Value()
+        name = property.Utf8Value()
+        dontintercept = proc do
+          return V8::C::Value::Empty
+        end
+        access.set(object, name, context.to_ruby(value), &dontintercept)
       rescue Exception => e
         warn "uncaught exception: #{e.class}: #{e.message} while accessing object property: #{e.backtrace.join('\n')}"
       end
