6a306a93db2b42fc69574a16ad2d8f528cba7ecc
Charles Lowell <cowboyd@thefrontside.net>
2012-06-15 05:43:12 -0500

    construct valid instances

--- lib/v8/conversion/class.rb
+++ lib/v8/conversion/class.rb
@@ -11,6 +11,10 @@ class V8::Conversion
     def to_v8_template
       weakcell(:v8_constructor) do
         template = V8::C::FunctionTemplate::New(Constructor.new(self))
+        prototype = template.InstanceTemplate()
+        prototype.SetNamedPropertyHandler(Get, Set)
+        prototype.SetIndexedPropertyHandler(IGet, ISet)
+        template
       end
     end
 
@@ -32,5 +36,59 @@ class V8::Conversion
         end
       end
     end
+
+    module Accessor
+      include V8::Error::Protect
+      def intercept(info, key, &block)
+        context = V8::Context.current
+        access = context.access
+        object = info.Data().Value()
+        handles_property = true
+        dontintercept = proc do
+          handles_property = false
+        end
+        protect do
+          result = block.call(context, access, object, context.to_ruby(key), dontintercept)
+          handles_property ? context.to_v8(result) : V8::C::Value::Empty
+        end
+      end
+    end
+
+    class Get
+      extend Accessor
+      def self.call(property, info)
+        intercept(info, property) do |context, access, object, key, dontintercept|
+          access.get(object, key, &dontintercept)
+        end
+      end
+    end
+
+    class Set
+      extend Accessor
+      def self.call(property, value, info)
+        intercept(info, property) do |context, access, object, key, dontintercept|
+          access.set(object, key, context.to_ruby(value), &dontintercept)
+        end
+      end
+    end
+
+    class IGet
+      extend Accessor
+      def self.call(property, info)
+        intercept(info, property) do |context, access, object, key, dontintercept|
+          access.iget(object, key, &dontintercept)
+        end
+      end
+    end
+
+    class ISet
+      extend Accessor
+      def self.call(property, value, info)
+        intercept(info, property) do |context, access, object, key, dontintercept|
+          access.iset(object, key, context.to_ruby(value), &dontintercept)
+        end
+      end
+    end
+
   end
 end
