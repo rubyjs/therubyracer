822b1eb7599a7260dc4bbbc37d70f734639eddfa
Charles Lowell <cowboyd@thefrontside.net>
2012-06-15 05:33:18 -0500

    factor out common property interceptor code.

--- lib/v8/conversion/object.rb
+++ lib/v8/conversion/object.rb
@@ -22,64 +22,55 @@ class V8::Conversion
       self
     end
 
-    class Get
-      extend V8::Error::Protect
-      def self.call(property, info)
+    module Accessor
+      include V8::Error::Protect
+      def intercept(info, key, &block)
+        context = V8::Context.current
+        access = context.access
+        object = info.Data().Value()
+        handles_property = true
+        dontintercept = proc do
+          handles_property = false
+        end
         protect do
-          context = V8::Context.current
-          access = context.access
-          object = info.Data().Value()
-          name = property.Utf8Value()
-          dontintercept = proc do
-            return V8::C::Value::Empty
-          end
-          context.to_v8 access.get(object, name, &dontintercept)
+          result = block.call(context, access, object, context.to_ruby(key), dontintercept)
+          handles_property ? context.to_v8(result) : V8::C::Value::Empty
+        end
+      end
+    end
+
+    class Get
+      extend Accessor
+      def self.call(property, info)
+        intercept(info, property) do |context, access, object, key, dontintercept|
+          access.get(object, key, &dontintercept)
         end
       end
     end
 
     class Set
-      extend V8::Error::Protect
+      extend Accessor
       def self.call(property, value, info)
-        protect do
-          context = V8::Context.current
-          access = context.access
-          object = info.Data().Value()
-          name = property.Utf8Value()
-          dontintercept = proc do
-            return V8::C::Value::Empty
-          end
-          access.set(object, name, context.to_ruby(value), &dontintercept)
+        intercept(info, property) do |context, access, object, key, dontintercept|
+          access.set(object, key, context.to_ruby(value), &dontintercept)
         end
       end
     end
 
     class IGet
-      extend V8::Error::Protect
-      def self.call(index, info)
-        protect do
-          context = V8::Context.current
-          access = context.access
-          object = info.Data().Value()
-          dontintercept = proc do
-            return V8::C::Value::Empty
-          end
-          context.to_v8 access.iget(object, index, &dontintercept)
+      extend Accessor
+      def self.call(property, info)
+        intercept(info, property) do |context, access, object, key, dontintercept|
+          access.iget(object, key, &dontintercept)
         end
       end
     end
 
     class ISet
-      extend V8::Error::Protect
-      def self.call(index, value, info)
-        protect do
-          context = V8::Context.current
-          access = context.access
-          object = info.Data().Value()
-          dontintercept = proc do
-            return V8::C::Value::Empty
-          end
-          access.iset(object, index, context.to_ruby(value), &dontintercept)
+      extend Accessor
+      def self.call(property, value, info)
+        intercept(info, property) do |context, access, object, key, dontintercept|
+          access.iset(object, key, context.to_ruby(value), &dontintercept)
         end
       end
     end
