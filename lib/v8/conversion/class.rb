7bc06b27c0e72656338e957e3a5a84a01221344e
Charles Lowell <cowboyd@thefrontside.net>
2012-06-14 23:11:21 -0500

    embed classes as constructors

--- lib/v8/conversion/class.rb
+++ lib/v8/conversion/class.rb
@@ -4,8 +4,27 @@ class V8::Conversion
 
     def to_v8
       weakcell(:v8_constructor) do
-        V8::C::FunctionTemplate::New()
+        V8::C::FunctionTemplate::New(Constructor.new(self))
       end.GetFunction()
+    end
+
+    class Constructor
+      include V8::Error::Protect
+
+      def initialize(cls)
+        @class = cls
+      end
+
+      def call(arguments)
+        protect do
+          context = V8::Context.current
+          args = ::Array.new(arguments.Length())
+          0.upto(args.length - 1) do |i|
+            args[i] = context.to_ruby arguments[i]
+          end
+          context.to_v8 @class.new(*args)
+        end
+      end
     end
   end
 end
