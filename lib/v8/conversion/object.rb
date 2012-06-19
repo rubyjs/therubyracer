1ce63c508cba3b4658b039caf7acd807ec5a9299
Charles Lowell <cowboyd@thefrontside.net>
2012-06-19 04:42:04 -0500

    extract default object conversion

--- lib/v8/conversion/object.rb
+++ lib/v8/conversion/object.rb
@@ -1,10 +1,7 @@
 class V8::Conversion
   module Object
     def to_v8
-      context = V8::Context.current
-      constructor = context.to_v8(self.class)
-      object = constructor.NewInstance([V8::C::External::New(self)])
-      return object
+      Reference.construct! self
     end
 
     def to_ruby
