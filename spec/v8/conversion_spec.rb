c5b13ea1150ffc3a14b48e8dec5d04cc0e486a0c
Charles Lowell <cowboyd@thefrontside.net>
2012-07-24 23:25:04 +0300

    Move Get,Set,Constructor and friends out of Class
    Even though they had been moved into the
    `ClassAction` module, that module still had the
    potential for conflict. Instead, we'll just put 
    evenything in the Conversion namespace. That way
    we need not worry.

--- spec/v8/conversion_spec.rb
+++ spec/v8/conversion_spec.rb
@@ -6,4 +6,16 @@ describe V8::Conversion do
     cxt['big'] = BigDecimal.new('1.1')
     cxt['big'].should eql BigDecimal.new('1.1')
   end
+
+  it "doesn't try to use V8::Conversion::Class::* as root objects" do
+    klass = Class.new do
+      class << self
+        def test
+          Set.new
+        end
+      end
+    end
+
+    klass.test.should be_instance_of(::Set)
+  end
 end
