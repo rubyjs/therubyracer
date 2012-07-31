531d5685bbc50ab24f6a02b3e9b1dbb92a38b6fc
Charles Lowell <cowboyd@thefrontside.net>
2012-07-31 19:56:04 +0300

    Revert "Conversion trick for 64-bit Fixnums"
    see http://travis-ci.org/#!/cowboyd/therubyracer/jobs/2002117
    
    This reverts commit fde0d8ae9a8a972abe861b6ca23efdd593cace8d.

--- spec/v8/conversion_spec.rb
+++ spec/v8/conversion_spec.rb
@@ -1,10 +1,8 @@
 require 'spec_helper'
 require 'bigdecimal'
 describe V8::Conversion do
-
-  let(:cxt) { V8::Context.new }
-
   it "can embed BigDecimal values" do
+    cxt = V8::Context.new
     cxt['big'] = BigDecimal.new('1.1')
     cxt['big'].should eql BigDecimal.new('1.1')
   end
@@ -19,34 +17,5 @@ describe V8::Conversion do
     end
 
     klass.test.should be_instance_of(::Set)
-  end
-
-  context "::Fixnum" do
-    context "for 32-bit numbers" do
-      it "should convert positive integer" do
-        cxt['fixnum_a'] = 0xFFFFFFFF
-        cxt['fixnum_a'].should == 0xFFFFFFFF
-        cxt['fixnum_a'].should be_instance_of(Fixnum)
-      end
-
-      it "should convert negative integer" do
-        cxt['fixnum_b'] = -0xFFFF
-        cxt['fixnum_b'].should == -0xFFFF
-        cxt['fixnum_b'].should be_instance_of(Fixnum)
-      end
-    end
-
-    context "for 64-bit numbers" do
-      it "should convert positive integer" do
-        cxt['fixnum_c'] = 0x100000000
-        cxt['fixnum_c'].should == 0x100000000
-      end
-
-      it "should convert negative integer" do
-        cxt['fixnum_d'] = -0xFFFFFFFF
-        cxt['fixnum_d'].should == -0xFFFFFFFF
-      end
-    end
-
   end
 end
